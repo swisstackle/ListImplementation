d4abf2a443100fa2be25070f3269ab002ea728bb
Alain Schaerer <alain.schaerer@netrio.com>
2026-06-07 13:11:11 -0400

    Implement structure. Implement getter setter

--- ListImplementation/MyList.cs
+++ ListImplementation/MyList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,33 @@ namespace ListImplementation
         private int _version;
         private static readonly T[] s_emptyArray = new T[0];
 
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public T this[int index]
+        {
+
+            get
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                return _items[index];
+            }
+
+            set
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                _items[index] = value;
+                _version++;
+            }
+        }
+
         public MyList()
         {
             _items = s_emptyArray;
@@ -26,6 +54,59 @@ namespace ListImplementation
                 var newCapacity = _items.Length == 0 ? 4 : _items.Length * 2;
                 Array.Resize(ref _items, newCapacity);
             }
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            Grow();
+            _items[_size] = item;
+            _size++;
+            _version++;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
