0043d1890ccd01d6b51b88ffad114dbd6da4b363
Alain Schaerer <alain.schaerer@netrio.com>
2026-06-07 12:09:58 -0400

    Initial Commit

---
+++ ListImplementation/MyList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListImplementation
+{
+    public class MyList<T> : IList<T>, ICollection<T>, IEnumerable<T>
+    {
+        private T[] _items;
+        private int _size;
+        private int _version;
+        private static readonly T[] s_emptyArray = new T[0];
+
+        public MyList()
+        {
+            _items = s_emptyArray;
+            _size = 0;
+        }
+    }
+}
