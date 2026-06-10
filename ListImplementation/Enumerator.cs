edfcadda832274ba22c8942c989726c035ffab70
Alain Schaerer <alain.schaerer@netrio.com>
2026-06-09 21:35:31 -0400

    Add custom Enumerator struct for MyList

---
+++ ListImplementation/Enumerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListImplementation
+{
+    public struct Enumerator<T> : IEnumerator<T>, IEnumerator
+    {
+
+        private readonly MyList<T> _list;
+        private int _index;
+        private readonly int _version;
+        public T? Current;
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        T IEnumerator<T>.Current => throw new NotImplementedException();
+
+
+        internal Enumerator(MyList<T> list)
+        {
+            _list = list;
+            _version = 0;
+            _index = 0;
+            Current = default;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
